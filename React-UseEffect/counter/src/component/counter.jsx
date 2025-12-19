79092935bff7108b289dd6f136cb95e7ae013464
Nikita-Parjane27 <145220659+Nikita-Parjane27@users.noreply.github.com>
2025-12-19 22:55:27 +0530

    update

---
+++ React-UseEffect/counter/src/component/counter.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]); 
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
